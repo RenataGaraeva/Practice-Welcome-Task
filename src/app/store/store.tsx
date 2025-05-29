61bd792a5a0a4e149b742119bebafb4279bfd25f
RenataGaraeva <renatagaraeva12@gmail.com>
2025-05-29 14:19:56 +0300

    настройка проекта. Создала Next.js приложение

---
+++ src/app/store/store.tsx
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import tasksReducer from "@features/TasksSlice";
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
---
+++ src/features/add_task_form/ui/index.tsx
@@ -0,0 +1,21 @@
+export default function AddTaskForm() {
+
+    return (
+        <>
+            <form
+               /* onSubmit={handleSubmit}
+                className="mb-3 mt-3 d-flex justify-content-center"
+
+                */
+            >
+                <input
+                    type="text"
+                    id="taskTitle"
+               /*     className="form-control me-2 w-50" */
+                    placeholder="Enter task"
+                />
+                <input /*className="btn btn-outline-primary" */ type="submit" value="Add" />
+            </form>
+        </>
+    );
+}
