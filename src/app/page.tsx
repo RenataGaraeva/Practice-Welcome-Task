7aac2ed98d029ab5ae6f6e36205101f2078ae539
RenataGaraeva <renatagaraeva12@gmail.com>
2025-06-01 20:44:42 +0300

    добавила на страницу компонент для светлой/темной темы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AddTaskForm from "@features/add_task_form/ui";
 import Tasks from "@entities/tasks_list/ui";
+import Theme_light_dark from "@entities/theme/ui";
 
 export default function Home() {
   return (
     <div>
       <main>
+          <Theme_light_dark />
           <AddTaskForm />
           <Tasks />
       </main>
