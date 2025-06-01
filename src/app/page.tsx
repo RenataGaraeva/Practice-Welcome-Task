f9a8f304a544631f3ba3bb7d031f1c16ce94310c
RenataGaraeva <renatagaraeva12@gmail.com>
2025-06-01 22:52:14 +0300

    Настроила Bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import AddTaskForm from "@features/add_task_form/ui";
 import Tasks from "@entities/tasks_list/ui";
 import Theme_light_dark from "@entities/theme/ui";
+import "./scss.scss";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
