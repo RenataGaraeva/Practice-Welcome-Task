dd6a98d983832c899cbe667b81134452fc72d97b
RenataGaraeva <renatagaraeva12@gmail.com>
2025-06-01 16:52:15 +0300

    добавила компоненты формы и списка задач на главную страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import AddTaskForm from "@features/add_task_form/ui";
+import Tasks from "@entities/tasks_list/ui";
+
 export default function Home() {
   return (
     <div>
       <main>
+          <AddTaskForm />
+          <Tasks />
       </main>
     </div>
   );
