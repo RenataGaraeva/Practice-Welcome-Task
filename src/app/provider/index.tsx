d9690ef3548a6df8a6453cc5085b5372e4e9e6fc
RenataGaraeva <renatagaraeva12@gmail.com>
2025-06-01 17:19:04 +0300

    добавила provider

---
+++ src/app/provider/index.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from '@app/store';
+
+type Props = {
+    children: ReactNode;
+}
+
+export const ProviderStore = ({ children }: Props) => {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
